78e93879cf5e2a972e941d9a10837e5f2ef29cd8
杨杰 <616651001@qq.com>
2018-03-13 10:04:22 +0800

    完成了底部和购物车的效果

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
 <template>
   <div id="app">
     
+    <!-- 头部组件 -->
     <nav-header></nav-header>
 
     <router-view/>           
     <!-- 路由，根据我们的路径，可以替换不同的内容 -->
+
+    <!-- 底部组件 -->
+    <nav-footer></nav-footer>
+    <!-- <nav-footer /> -->
   </div>
 </template>
 
@@ -14,11 +19,13 @@
 // 导入新建好的组件
 // import  ES6 导入的代码，是关键字，类似于 var
 import NavHeader from "./components/NavHeader"
+import NavFooter from "./components/NavFooter"
 
 export default {
   name: 'App',
   components: {  
-    NavHeader
+    NavHeader,
+    NavFooter
   }
 }
 </script>
@@ -30,6 +37,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+  /* 注释： Ctrl+/ */
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 //   @ 代表我们项目文件 src 此目录
 import HelloWorld from '@/components/HelloWorld'
 import ProjectList from '@/page/ProjectList'
+import Cart from '@/page/Cart'
 
 Vue.use(Router)
 
@@ -13,6 +14,11 @@ export default new Router({
       path: '/',                    // 路径，根路径
       name: 'ProjectList',           // 路由名字
       component: ProjectList         // 使用哪个组件
+    },
+    {
+      path: '/cart',                  // 购物车
+      name: 'Cart',                   // 路由名字
+      component: Cart          // 使用哪个组件
     }
   ]
 })
